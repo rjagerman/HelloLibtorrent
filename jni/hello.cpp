9c52eed19763cf766ca7e41aa0159a44d3efea54
Rolf Jagerman <rjagerman@gmail.com>
2014-06-03 16:44:10 +0200

    Added a very simple torrent client application

--- jni/hello.cpp
+++ jni/hello.cpp
@@ -1,167 +1,63 @@
 /*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * A simple libtorrent client for android using JNI
  */
 #include <string>
 #include <jni.h>
-
-#include <boost/filesystem/operations.hpp>
-#include <boost/filesystem/fstream.hpp>
-
 #include <libtorrent/torrent_handle.hpp>
 #include <libtorrent/session.hpp>
 #include <libtorrent/add_torrent_params.hpp>
 #include <libtorrent/torrent_info.hpp>
 
-// A handle to the torrent that will be downloaded
+// Expose our JNI functions
+extern "C" {
+	jstring Java_com_example_hellolibtorrent_HelloLibtorrent_startTorrent( JNIEnv* env, jobject thiz );
+	jstring Java_com_example_hellolibtorrent_HelloLibtorrent_getStatus( JNIEnv* env, jobject thiz );
+}
+
+// Set variables
+char* torrent_file = "/storage/extSdCard/Torrents/ubuntu-14.04-desktop-i386.iso.torrent";
+char* save_location = "/storage/extSdCard/Torrents/";
+libtorrent::session s;
+libtorrent::add_torrent_params p;
 libtorrent::torrent_handle h;
 
-// The file path (as a string) of the torrent that will be downloaded
-std::string torrent_to_download;
+/* 
+ * Starts downloading the torrent and returns the path of the torrent file we are downloading
+ */
+jstring Java_com_example_hellolibtorrent_HelloLibtorrent_startTorrent( JNIEnv* env, jobject thiz ) {
+	std::string result = "";
 
-// Open libtorrent session
-libtorrent::session s;
+	try {
+		s.listen_on(std::make_pair(6881, 6889));
+    	p.save_path = save_location;
+    	p.ti = new libtorrent::torrent_info(torrent_file);
+    	h = s.add_torrent(p);
 
-// The torrent parameters
-libtorrent::add_torrent_params p;
+    	result = "Started torrent";
+    } catch(std::exception& e) {
+    	result = e.what();
+    }
 
-extern "C" {
+	return env->NewStringUTF(result.c_str());
+}
 
-	/* 
-	 * Prints the torrent that we will download
-	 */
-	jstring Java_com_example_hellolibtorrent_HelloLibtorrent_whichTorrent( JNIEnv* env, jobject thiz ) {
-		torrent_to_download = "/storage/sdcard0/Torrents/ubuntu-14.04-desktop-i386.iso.torrent";
-		return env->NewStringUTF(torrent_to_download.c_str());
+/*
+ * Returns the current status of the torrent in a single string
+ */
+jstring Java_com_example_hellolibtorrent_HelloLibtorrent_getStatus( JNIEnv* env, jobject thiz ) {
+	std::stringstream result;
+
+	if (h.is_valid()) {
+		libtorrent::torrent_status status = h.status();
+		result << "Progress: " << (status.progress * 100.0) << "%" << std::endl
+		    << " Download rate: " << status.download_rate << std::endl
+		    << " Upload rate: " << status.upload_rate << std::endl
+		    << " Seeds: " << status.num_seeds << std::endl
+		    << " Peers: " << status.num_peers;
+	} else {
+		result << "Handle is not valid";
 	}
-
-	/*
-	 * Starts listening on a libtorrent session 
-	 */
-	jstring Java_com_example_hellolibtorrent_HelloLibtorrent_startSession( JNIEnv* env, jobject thiz ) {
-		std::string result = "Started libtorrent session";
-
-		try {
-			
-			// Start listening
-			s.listen_on(std::make_pair(6881, 6889));
-
-		} catch (std::exception& e) {
-			result = e.what();
-		}
-
-		return env->NewStringUTF(result.c_str());
-	}
-
-	jstring Java_com_example_hellolibtorrent_HelloLibtorrent_openTorrent( JNIEnv* env, jobject thiz ) {
-		std::string result = "Loaded torrent file";
-
-		try {
-
-			// Set torrent info and load the torrent file
-			p.save_path = "/storage/sdcard0/Torrents/";
-			p.ti = new libtorrent::torrent_info(torrent_to_download);
-
-			// Get torrent size
-			std::stringstream ss;
-			ss  << "Opened torrent:\n"
-				<< "  Size: " << p.ti->total_size() << " bytes\n"
-				<< "  Save path: " << p.save_path;
-			result = ss.str();
-
-
-		} catch (std::exception& e) {
-			result = e.what();
-		}
-
-		return env->NewStringUTF(result.c_str());
-	}
-
-	/*
-	 * Starts the download and returns a status message or error message
-	 */
-	jstring Java_com_example_hellolibtorrent_HelloLibtorrent_startDownload( JNIEnv* env, jobject thiz ) {
-
-		std::string result = "nothing";
-		try {
-
-			// Start torrent
-			h = s.add_torrent(p);
-			result = "Added torrent to session";
-
-		} catch (std::exception& e) {
-			result = e.what();
-		}
-
-		return env->NewStringUTF(result.c_str());
-	}
-
-	/*
-	 * Gets the progress on the torrent download
-	 */
-	jstring Java_com_example_hellolibtorrent_HelloLibtorrent_status( JNIEnv* env, jobject thiz ) {
-		std::string output = "???";
-		try {
-			if (!h.is_valid()) {
-				output = "Handle is not valid...";
-			} else {
-				// Get torrent status
-				libtorrent::torrent_status status = h.status();
-
-				// Convert torrent state from enum to string
-				std::string stringstate = "Unknown state";
-				switch(status.state) {
-					case libtorrent::torrent_status::queued_for_checking:
-						stringstate = "Queued for checking"; break;
-					case libtorrent::torrent_status::checking_files:
-						stringstate = "Checking files"; break;
-					case libtorrent::torrent_status::downloading_metadata:
-						stringstate = "Downloading metadata"; break;
-					case libtorrent::torrent_status::downloading:
-						stringstate = "Downloading"; break;
-					case libtorrent::torrent_status::finished:
-						stringstate = "Finished"; break;
-					case libtorrent::torrent_status::seeding:
-						stringstate = "Seeding"; break;
-					case libtorrent::torrent_status::allocating:
-						stringstate = "Allocating"; break;
-					case libtorrent::torrent_status::checking_resume_data:
-						stringstate = "Checking resume data"; break;
-					default:
-						stringstate = "Unknown state";
-				}
-
-				// Get torrent status and print it in string format
-				std::stringstream ss;
-				ss  << "Torrent status\n"
-				    << "  Name: " << status.name << "\n"
-				    << "  Error: " << status.error << "\n"
-					<< "  State: " << stringstate << "\n"
-					<< "  Progress: " << (status.progress * 100.0) << "%\n"
-					<< "  Seeds: " << status.list_seeds << " (Active: " << status.num_seeds << ")\n"
-					<< "  Peers: " << status.list_peers << " (Active: " << status.num_peers << ")\n"
-					<< "  Download rate: " << status.download_rate << " bps\n"
-					<< "  Upload rate: " << status.upload_rate << " bps\n";
-				output = ss.str();
-			}
-
-		} catch(std::exception& e) {
-			output = e.what();
-		}
-		return env->NewStringUTF(output.c_str());
-	}
-
+	
+	return env->NewStringUTF(result.str().c_str());
 }
 
--- src/com/example/hellolibtorrent/HelloLibtorrent.java
+++ src/com/example/hellolibtorrent/HelloLibtorrent.java
@@ -1,163 +1,60 @@
 /*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Simple example of a torrent client on android
  */
 package com.example.hellolibtorrent;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
-public class HelloLibtorrent extends Activity
-{
-    /**
-     * The output TextView that we print to
-     */
-    private TextView output;
-
-    /**
-     * The total text in the output text view, to easily append to
-     */
-    private String currentText = "";
+public class HelloLibtorrent extends Activity {
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Create vertical layout
-        LinearLayout ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.VERTICAL);
+        // Create text area with scroll
+        final TextView textView = new TextView(this);
+        ScrollView scrollView = new ScrollView(this);
+        scrollView.addView(textView);
+        this.setContentView(scrollView);
 
-        // Create button
-        final Button b = new Button(this);
-        b.setText("Start");
-        ll.addView(b);
+        // Start torrent and output torrent name to screen
+        String torrent_name = startTorrent();
+        textView.setText(torrent_name);
 
-        // Create text area (with scroll)
-        ScrollView sv = new ScrollView(this);
-        output = new TextView(this);
-        sv.addView(output);
-        ll.addView(sv);
-
-        // Set vertical layout as main view
-        this.setContentView(ll);
-
-        // Get the torrent to download and print it
-        appendToOutput("We are going to download:\n  " + whichTorrent(), true);    
-
-        // Start session
-        appendToOutput(startSession(), true);
-
-        // Open torrent file
-        appendToOutput(openTorrent(), true);    
-
-        // On click, print the current progress of the download to the output
-        b.setOnClickListener(new OnClickListener() {
-            private boolean started = false;
-            @Override
-            public void onClick(View v) {
-                if(!started) {
-
-                    // Start the download and print the status
-                    appendToOutput(startDownload(), true);
-
-                    // Set a schedule for getting progress updates
-                    new Timer().scheduleAtFixedRate(new TimerTask() {
-                        private int updates = 0;
-                        @Override
-                        public void run() {
-                            updates++;
-                            final String status = status();
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    appendToOutput("Update #" + updates + "\n" + status, false);
-                                }
-                            });
-                        }
-                    }, 0, 3);
-
-                    // Ensure this function doesn't get called again
-                    started = true;
-                    b.setText("Downloading...");
-                    b.setEnabled(false);
-                }
-            }
-        });
-        new Thread(new Runnable() {
+        // Get updates at a regular interval and print them to the screen
+        new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(1500);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            b.performClick();
-                        }
-                    });
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String status = getStatus();
+                        textView.setText(status);
+                    }
+                });
             }
-        }).start();
-        
-    }
+        }, 0, 5);
 
-    /* Appends given text to the output TextView. 
-     */
-    public void appendToOutput(String text, boolean addPermanently) {
-        if (addPermanently) {
-            currentText += text + "\n\n";
-            output.setText(currentText);
-        } else {
-            output.setText(currentText + text);
-        }
+
     }
 
     /* A native method that tells us which torrent file we are going to download
      */
-    public native String  whichTorrent();
+    public native String  startTorrent();
 
     /* A native method that starts the download of the hardcoded torrent
      */
-    public native String startSession();
-
-    /* A native method that starts the download of the hardcoded torrent
-     */
-    public native String openTorrent();
-
-    /* A native method that starts the download of the hardcoded torrent
-     */
-    public native String startDownload();
-
-    /* A native method that returns the status of the torrent download
-     */
-    public native String status();
+    public native String getStatus();
 
     /* Which libraries to include and their dependencies
      * Note that the order is very important. Place libraries that are not dependent
-     * on anything first, and only place dependen libraries after the dependencies have
+     * on anything first, and only place dependent libraries after the dependencies have
      * already been included. 
      */
     static {
